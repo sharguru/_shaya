34f3e84a262d7fa6a46321c9f627477c033113fb
sharguru <46201383+sharguru@users.noreply.github.com>
2019-05-28 14:11:10 +0530

    Update pro3.py

--- pro3.py
+++ pro3.py
@@ -1,6 +1,9 @@
 s=str(input())
 l=['a','e','i','o','u']
+l1=['b','c','d','f','i','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 if s in l:
   print("Vowel")
-else:
+elif s in l1:
   print("Consonant")
+else:
+  print("Invalid") 
