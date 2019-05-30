8c7da27be47d8ac5f4ecf13111cc325e8cfc2146
sharguru <46201383+sharguru@users.noreply.github.com>
2019-05-30 11:12:12 +0530

    Create pro4.py

---
+++ pro4.py
@@ -0,0 +1,6 @@
+l=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+a=str(input())
+if a in l:
+    print("Alphabet")
+else:
+    print("No")
